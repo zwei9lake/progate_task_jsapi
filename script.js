3709c9ab365e6087e4d194b58a1f6280d8613c24
zwei9lake <a9subiyanto@gmail.com>
2022-06-22 15:20:40 +0700

    finishing project, tambah js dan css

--- script.js
+++ script.js
@@ -11,12 +11,22 @@ function getAnime(e) {
     .then((response) => response.json())
     .then((data) => {
       document.querySelector(".detailBox").innerHTML = `
-      <div>
+      
+      <div class= "my-3">
+        <img
+          src="${data.data[0].images.jpg.image_url}"
+          alt="Pokemon name"
+          class="img-fluid mx-auto d-block my-3"
+        />
         
-      </div>
-      <div class="animeInfo">
-        <h1>${data.data[0].title}</h3>
-        <p>Weight: ${data.title}</p>
+        <div class="card w-75" style="margin:0 auto;">
+          <div class="card-body shadow bg-body rounded">
+            <h1>${data.data[0].title}</h1>
+            <p class = "small">${data.data[0].genres[0].name}, ${data.data[0].genres[1].name}</p>
+            <p style="text-indent: 50px;">${data.data[0].synopsis}</p>
+            <a href="${data.data[0].url}" class="btn btn-primary">Visit Official Site</a>
+          </div>
+        </div>
       </div>`;
     })
     .catch((err) => {
